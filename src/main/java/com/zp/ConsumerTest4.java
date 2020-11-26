e3eb3414c40b208ac0dba0c26abc465455cb4fb0
zhoupeng <287214764@qq.com>
2020-11-26 14:39:27 +0800

    消费者手动提交offset

---
+++ src/main/java/com/zp/ConsumerTest4.java
@@ -0,0 +1,44 @@
+package com.zp;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @Author zp
+ * @create 2020/11/26 10:57
+ */
+public class ConsumerTest4 {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.129.149:9092");
+        // 关闭自动提交offset
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        // 自动提交的延时
+//        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        // 反序列化
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        // 消费者组,与第一个消费者不同组
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test2");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        // 订阅主题
+        consumer.subscribe(Arrays.asList("topic001"));
+        while (true) {
+            // 获取数据
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(100);
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord.toString());
+            }
+            // 手动异步提交offset
+            consumer.commitAsync();
+        }
+//        consumer.close();
+
+    }
+}
